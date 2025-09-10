c13777f1efdfdcd54b27376fab0405182b4b20ab
Mkjangid <mahendrakumar24325@gmail.com>
2025-09-10 16:28:54 +0530

    commit

--- scraper.py
+++ scraper.py
@@ -3,13 +3,11 @@ import pandas as pd
 
 
 def get_stock_data(ticker="TCS.NS", period="1mo", interval="1d"):
-    """Fetch historical stock data from Yahoo Finance"""
     stock = yf.Ticker(ticker)
     hist = stock.history(period=period, interval=interval)
     return hist.reset_index()
 
 
 def get_stock_info(ticker="TCS.NS"):
-    """Fetch company info & current market data"""
     stock = yf.Ticker(ticker)
     return stock.info
